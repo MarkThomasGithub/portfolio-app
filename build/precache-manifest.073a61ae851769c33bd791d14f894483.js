f30828c8e97f3f4b9e4a2b193389d6740881661f
MarkThomasGithub <mark.e.thomas.1997@gmail.com>
2019-12-06 20:54:09 +1100

    serve static html rather than dynamic

---
+++ build/precache-manifest.073a61ae851769c33bd791d14f894483.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbfdfbb35abe060ff2762d30cbec453",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4aca7940b3c8845b1e1",
+    "url": "/static/css/main.68b2844d.chunk.css"
+  },
+  {
+    "revision": "366bba6f09a05a73b7c6",
+    "url": "/static/js/2.d4b27b0a.chunk.js"
+  },
+  {
+    "revision": "dd47e78386a53fd0f632c0884cee616b",
+    "url": "/static/js/2.d4b27b0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4aca7940b3c8845b1e1",
+    "url": "/static/js/main.bbc5dbe8.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  }
+]);
--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetProjects } from './actions/projects'
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
 import './firebase/firebase';
 
 const store = configureStore();
@@ -17,15 +15,19 @@ const state = store.getState();
 
 //console.log(store.getState());
 
-const jsx = (
-    <Provider store={store}>
-        <AppRouter />  
-    </Provider>
-);
+export default class App extends React.Component {
+    render(){
+        return (
+            <Provider store={store}>
+                <AppRouter />  
+            </Provider>
+        );
+    }
+}
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
 
 store.dispatch(startSetProjects()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'));
+    ReactDOM.render(<App />, document.getElementById('root'));
 });
 
